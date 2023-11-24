d65436aff3859ba2526d266e7b498e756550fed3
Otavio Santana <otaviopolianasantana@gmail.com>
2023-11-24 19:53:43 +0000

    feat: create app hava
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- chapter7/src/main/java/expert/os/App.java
+++ chapter7/src/main/java/expert/os/App.java
@@ -1,4 +1,15 @@
 package expert.os;
 
 public class App {
+    public static void main(String[] args) {
+        String str = "Native Image is awesome";
+        String reversed = reverseString(str);
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str) {
+        if (str.isEmpty())
+            return str;
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
 }
