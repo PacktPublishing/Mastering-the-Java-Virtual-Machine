4b6fafddaeeba8c367357dbd2c6b1cc1fbd1859c
Otavio Santana <otaviopolianasantana@gmail.com>
2024-01-23 20:16:12 +0000

    feat: include math class at the chapter 5#
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ chapter5/src/main/java/expert/os/Match.java
@@ -0,0 +1,20 @@
+package expert.os;
+
+public class Math {
+
+    int sum(int a, int b) {
+        return a + b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    double subtract(double a, int b) {
+        return a - b;
+    }
+
+    static double divide(double a, long b) {
+        return a + b;
+    }
+}
